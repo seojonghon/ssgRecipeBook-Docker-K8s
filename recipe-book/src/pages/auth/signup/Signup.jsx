9aecc7f161219070be5f30dff9efea34f208d5e9
seojonghon <seojonghon@naver.com>
2023-10-27 16:21:23 +0900

    feat Post.js파일 댓글 보이기 기능 추가

--- recipe-book/src/pages/auth/signup/Signup.jsx
+++ recipe-book/src/pages/auth/signup/Signup.jsx
@@ -1,11 +1,44 @@
-function Signup() {
-  return (
-    <>
-      {/* 
-        회원 가입 페이지 구현
-      */}
-    </>
-  );
-}
+import React, { useState } from 'react';
+import axios from 'axios';
 
+function Signup() {
+    const [userData, setUserData] = useState({
+        username: '',
+        password: '',
+        email: ''
+    });
+
+    const { username, password, email } = userData;
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setUserData({
+            ...userData,
+            [name]: value
+        });
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('https://rest-recipe-book-dptb.run.goorm.site/members/forms', userData);
+            if (response.data.result === 'success') {
+                alert('회원 가입 성공');
+            }
+            else {
+                alert('회원 가입 실패: ' + response.data.message);
+            }
+        } catch (error) {
+            console.error('회원 가입 요청 중 에러 발생 :', error);
+        }
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" name="username" value={username} onChange={handleChange} placeholder="유저네임 (ID)" required />
+            <input type="password" name="password" value={password} onChange={handleChange} placeholder="패스워드" required />
+            <input type="email" name="email" value={email} onChange={handleChange} placeholder="email@email.com" required />
+            <button type="submit">회원 가입</button>
+        </form>
+    );
+}
 export default Signup;
--- recipe-book/src/routes/Routing.jsx
+++ recipe-book/src/routes/Routing.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-import Login from "../pages/auth/login/Login";
 import Signup from "../pages/auth/signup/Signup";
 import Post from '../pages/post/Post';
 
@@ -7,10 +6,7 @@ function Routing() {
   return (
     <Routes>
       <Route path="/" element={<></>} />
-      <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
-      {/* 라우팅 추가 */}
       <Route path="/posts/:id" element={<Post />} />
     </Routes>
   );
